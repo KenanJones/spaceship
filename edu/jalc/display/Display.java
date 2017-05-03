722528ea361bc112cf4769171a37984641acc77d
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-05-03 08:28:33 -0500

    display

---
+++ edu/jalc/display/Display.java
@@ -0,0 +1,18 @@
+package edu.jalc.display;
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.util.*;
+
+public class Display extends JPanel{
+
+   public Display(){
+
+   }
+
+   @Override
+   public void paintComponent(Graphics g){
+      super.paintComponent(g);
+   }
+}
