3dfffa92b0be7445eb401b67d6a8e896b714c9af
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-05-03 08:49:00 -0500

    more display

---
+++ edu/jalc/display/BackgroundImage.java
@@ -0,0 +1,9 @@
+package edu.jalc.display;
+
+import java.awt.image.BufferedImage;
+
+public class BackgroundImage extends BufferedImage{
+  public BackgroundImage(){
+    
+  }
+}
--- edu/jalc/display/Display.java
+++ edu/jalc/display/Display.java
@@ -2,6 +2,7 @@ package edu.jalc.display;
 
 import javax.swing.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
 import java.awt.*;
 import java.util.*;
 
@@ -14,5 +15,7 @@ public class Display extends JPanel{
    @Override
    public void paintComponent(Graphics g){
       super.paintComponent(g);
+      g.drawImage(new BufferedImage(10,10,0),10,10,null);
+      //g.drawLine(0,0,10,10);
    }
 }
